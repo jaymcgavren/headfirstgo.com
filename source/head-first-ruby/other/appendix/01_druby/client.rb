2ae42e2977bf0095c2a4317b65372294c2fb3021
Jay McGavren <jay@mcgavren.com>
2019-03-06 12:11:32 -0700

    Delete Head First Ruby code samples

--- source/head-first-ruby/other/appendix/01_druby/client.rb
+++
@@ -1,5 +0,0 @@
-require 'drb/drb'
-DRb.start_service
-remote_object = DRbObject.new_with_uri("druby://localhost:8787")
-remote_object.push "hello", "network"
-p remote_object.last
--- source/head-first-ruby/other/appendix/01_druby/server.rb
+++
@@ -1,9 +0,0 @@
-require 'drb/drb'
-
-my_object = []
-DRb.start_service("druby://localhost:8787", my_object)
-20.times do
-  sleep 10
-  p my_object
-end
-DRb.thread.join
